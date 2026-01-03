d1920fd86d337ac2496cca451e473c8360f5d1d8
Noa Swimmer <netanoas@gmail.com>
2026-01-03 22:23:55 +0200

    add swagger dto support

--- src/fetch/dto/create-fetch.dto.ts
+++ src/fetch/dto/create-fetch.dto.ts
@@ -1,6 +1,8 @@
 import { IsArray, ArrayNotEmpty, IsUrl } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateFetchDto {
+  @ApiProperty({ type: [String], example: ['https://example.com'] })
   @IsArray()
   @ArrayNotEmpty()
   @IsUrl({ protocols: ['http', 'https'] }, { each: true })
